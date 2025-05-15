e33b43840fefc7a93c52fa1ff8179adf895384e4
ko-tarou <tkota6174@gmail.com>
2025-05-15 19:46:41 +0900

    Initial Commit

---
+++ DeadLineTests/DeadLineTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeadLineTests.swift
+//  DeadLineTests
+//
+//  Created by kota on 2025/05/15.
+//
+
+import Testing
+@testable import DeadLine
+
+struct DeadLineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
