c9700ec0273fbe73d7f7d32e62901ccb26ea9fd3
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-05-24 23:21:38 +0900

    表示のみ

--- DeadLine/Views/ShowItem.swift
+++ DeadLine/Views/ShowItem.swift
@@ -5,3 +5,48 @@
 //  Created by 三ツ井渚 on 2025/05/21.
 //
 
+import SwiftUI
+
+struct ShowItem: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var title: String
+    var date: String
+    var days: Int
+    var memo: String
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Text(title)
+                
+                Text(date)
+                
+                Text("\(days)")
+                Text("day")
+                
+                Text(memo)
+            
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("キャンセル") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "ellipsis")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ShowItem(title: "title", date: "2025/3/3", days: 20, memo: "memo")
+}
